eee94784d103cd0fbc7d54b0a00d359643fe1080
ElWillyGod <manuelantognazza@gmail.com>
2024-11-05 00:46:56 -0300

    User From

--- src/app/components/Users.tsx
+++ src/app/components/Users.tsx
@@ -4,14 +4,12 @@ import { customSignIn } from 'src/app/auth';
 import { SubmitButton } from 'src/app/submit-button';
 
 export default function Login() {
+    
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-gray-50">
       <div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border border-gray-100 shadow-xl">
         <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center sm:px-16">
-          <h3 className="text-xl font-semibold">Sign In</h3>
-          <p className="text-sm text-gray-500">
-            Inicie secion
-          </p>
+          <h3 className="text-xl font-semibold">Iniciar sesion</h3>
         </div>
         <Form
           action={async (formData: FormData) => {
@@ -19,7 +17,7 @@ export default function Login() {
             await customSignIn(formData.get('email') as string, formData.get('password') as string);
           }}
         >
-          <SubmitButton>Sign in</SubmitButton>
+          <SubmitButton>ingresar</SubmitButton>
           <p className="text-center text-sm text-gray-600">
             {"Don't have an account? "}
             <Link href="/register" className="font-semibold text-gray-800">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { Form } from 'src/app/fromLogin';
 import { customSignIn } from 'src/app/auth';
 import { SubmitButton } from 'src/app/submit-button';
@@ -8,18 +9,26 @@ export default function Login() {
     <div className="flex h-screen w-screen items-center justify-center bg-gray-50">
       <div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border border-gray-100 shadow-xl">
         <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center sm:px-16">
-          <h3 className="text-xl font-semibold">Iniciar sesion</h3>
+          <h3 className="text-xl font-semibold">Iniciar Sesion</h3>
         </div>
         <Form
           action={async (formData: FormData) => {
             'use server';
-            await customSignIn(formData.get('email') as string, formData.get('password') as string);
+            const result = await customSignIn(formData.get('email') as string, formData.get('password') as string);
+            if ('token' in result) {
+              console.log('Usuario autenticado:', result);
+              // Redirigir al usuario a la página de inicio
+              useRouter().push('/components/Users');
+            } else {
+              console.log(result);
+            }
+          
           }}
         >
           <SubmitButton>ingresar</SubmitButton>
           <p className="text-center text-sm text-gray-600">
             {"Don't have an account? "}
-            <Link href="http://localhost:5000/swagger/index.html" className="font-semibold text-gray-800">
+            <Link href="/register" className="font-semibold text-gray-800">
               Sign up
             </Link>
             {' for free.'}
